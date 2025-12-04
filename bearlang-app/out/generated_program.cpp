e8896483061c5be3223f0baacb0ba8e653704efd
swwwjjw <zhukegor2006@gmail.com>
2025-12-04 11:39:42 +0300

    Edited

--- bearlang-app/out/generated_program.cpp
+++ bearlang-app/out/generated_program.cpp
@@ -4,21 +4,7 @@
 
 int main() {
     std::ios_base::sync_with_stdio(false);
-    double vr_1{};
-    double vr_2{};
-    std::cout << "Введите первое число:" << std::endl;
-    std::cin >> vr_1;
-    std::cout << "Введите второе число:" << std::endl;
-    std::cin >> vr_2;
-    std::cout << "Сумма:" << std::endl;
-    std::cout << (vr_1 + vr_2) << std::endl;
-    std::cout << "Произведение:" << std::endl;
-    std::cout << (vr_1 * vr_2) << std::endl;
-    if ((vr_2 == 0)) {
-        std::cout << "На ноль делить нельзя" << std::endl;
-    }
-    else {
-        std::cout << (vr_1 / vr_2) << std::endl;
-    }
+    std::string vr_1 = "Мир";
+    std::cout << ("Привет, " + vr_1) << std::endl;
     return 0;
 }
